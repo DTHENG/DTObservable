e6aa107b174fa78617f85f3924ff4a260bf64958
DTHENG <fender5289@gmail.com>
2015-04-28 22:00:21 -0700

    missing files

---
+++ DTObservableExampleApp/DTObservableExampleApp/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  DTObservableExampleApp
+//
+//  Created by Daniel Thengvall on 4/28/15.
+//  Copyright (c) 2015 DTHENG. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ DTObservableExampleApp/Pods/Headers/Public/DTObservable/DTSubscriber.h
@@ -0,0 +1,1 @@
+../../../../../DTSubscriber.h
